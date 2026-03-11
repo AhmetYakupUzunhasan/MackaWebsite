a11c10b71a9d2fdbcd547a8b20a6a9c41165a15b
AhmetYakupUzunhasan <102602939+AhmetYakupUzunhasan@users.noreply.github.com>
2026-03-11 22:35:31 +0300

    Add full project files

--- internal/models/landing_page.go
+++ internal/models/landing_page.go
@@ -7,9 +7,21 @@ type LandingPage struct {
 	ImageLink string `json:"image_link" binding:"required"`
 }
 
+type PostLandingPage struct {
+	Title    string `json:"title" binding:"required"`
+	SubTitle string `json:"subtitle" binding:"required"`
+	Text     string `json:"text" binding:"required"`
+}
+
 type Blog struct {
 	Title     string `json:"title" binding:"required"`
 	SubTitle  string `json:"subtitle" binding:"required"`
 	Text      string `json:"text" binding:"required"`
 	ImageLink string `json:"image_link" binding:"required"`
+}
+
+type PostBlog struct {
+	Title    string `json:"title" binding:"required"`
+	SubTitle string `json:"subtitle" binding:"required"`
+	Text     string `json:"text" binding:"required"`
 }
